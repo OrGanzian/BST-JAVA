6b6cf06cacf7dc6fe97be864cf90d1a2b1fa24a3
OrGanzian <organzian10@gmail.com>
2021-06-30 12:07:05 +0300

    add notes and delet 'end class'

--- src/com/or/Main.java
+++ src/com/or/Main.java
@@ -5,10 +5,11 @@ public class Main {
 	
 	public static void main(String[] args) {
 
-	// add comments for testing
+	// add comments for testing 
+	// changes has made
 	}
 	
 
 		
 		
-}//class end
+}
